f17f3247455a3c801c050d29c8c0a2267de3753d
Will Ockelmann-Wagner <wow@carbonfive.com>
2019-02-11 22:05:12 -0800

    lint

--- fec/urls.py
+++ fec/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('politician/<int:politician_id>/', views.politician, name='politician'),
+    path(
+        'politician/<int:politician_id>/', views.politician,
+        name='politician'),
 ]
