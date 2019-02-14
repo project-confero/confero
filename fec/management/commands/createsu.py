7087d0aa8e9d828f4d2144f7db8b4824fb5b5b7d
Chris Garretty <cgarretty@gmail.com>
2019-02-13 22:50:29 -0500

    use enviro variables to create superuser.

--- fec/management/commands/createsu.py
+++ fec/management/commands/createsu.py
@@ -1,8 +1,10 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
+import os
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "admin@admin.com", "admin")
+            User.objects.create_superuser("cgarretty", "admin@admin.com",
+                                          os.environ['SUPERUSER_KEY'])
