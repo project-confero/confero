149ab586380c3d2c4c4d0f6ef4e6a8a47e3aa1d6
Will <will.ockelmann.wagner@gmail.com>
2020-01-25 21:24:05 -0800

    filter buttons -- needs cleanup

--- confero-front/src/components/candidates/Candidates.tsx
+++ confero-front/src/components/candidates/Candidates.tsx
@@ -9,17 +9,32 @@ import CandidateDetails from "./CandidateDetails";
 
 const Candidates = () => {
   const [filter, setFilter] = React.useState("");
+  const [officeFilters, setOfficeFilters] = React.useState<string[]>([]);
   const [
     selectedCandidate,
     setSelectedCandidate
   ] = React.useState<Candidate | null>(null);
 
+  const onOfficeClick = (office: string) => {
+    if (officeFilters.includes(office)) {
+      setOfficeFilters(officeFilters.filter(filter => filter !== office));
+    } else {
+      setOfficeFilters([...officeFilters, office]);
+    }
+  };
+
   const shownCandidates = React.useMemo(() => {
     const compareFilter = filter.toLowerCase();
-    return candidates.filter(candidate =>
-      candidate.name.toLowerCase().includes(compareFilter)
-    );
-  }, [filter]);
+    return candidates.filter(candidate => {
+      const matchesText = candidate.name.toLowerCase().includes(compareFilter);
+      const matchesOffice =
+        officeFilters.length > 0
+          ? officeFilters.includes(candidate.office)
+          : true;
+
+      return matchesText && matchesOffice;
+    });
+  }, [filter, officeFilters]);
 
   return (
     <Container>
@@ -28,6 +43,27 @@ const Candidates = () => {
         onChange={event => setFilter(event.target.value)}
         placeholder="Search by name"
       />
+
+      <Flex>
+        <Button
+          onClick={() => onOfficeClick("P")}
+          backgroundColor={officeFilters.includes("P") ? "primary" : "gray"}
+        >
+          President
+        </Button>
+        <Button
+          backgroundColor={officeFilters.includes("H") ? "primary" : "gray"}
+          onClick={() => onOfficeClick("H")}
+        >
+          House
+        </Button>
+        <Button
+          backgroundColor={officeFilters.includes("S") ? "primary" : "gray"}
+          onClick={() => onOfficeClick("S")}
+        >
+          Senate
+        </Button>
+      </Flex>
 
       <Flex>
         <Pagination size={10} items={shownCandidates}>
